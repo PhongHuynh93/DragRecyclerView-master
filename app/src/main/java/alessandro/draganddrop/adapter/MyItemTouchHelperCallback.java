e1a1d817b6ddf6c665975290f0a43166e9ed61f6
p7157568 <p7157568@gmail.com>
2016-02-06 15:17:14 +0700

    Initial commit

---
+++ app/src/main/java/alessandro/draganddrop/adapter/MyItemTouchHelperCallback.java
@@ -0,0 +1,47 @@
+package alessandro.draganddrop.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+
+import alessandro.draganddrop.interfaces.CallbackItemTouch;
+
+/**
+ * Created by Alessandro on 12/01/2016.
+ */
+public class MyItemTouchHelperCallback extends ItemTouchHelper.Callback {
+
+    CallbackItemTouch callbackItemTouch; // interface
+
+    public MyItemTouchHelperCallback(CallbackItemTouch callbackItemTouch){
+        this.callbackItemTouch = callbackItemTouch;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return false; // swiped disabled
+    }
+
+    // must override to determine the direction fo drag (UP or DOWN)
+    @Override
+    public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+        int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN; // movements drag
+        return makeFlag( ItemTouchHelper.ACTION_STATE_DRAG , dragFlags); // as parameter, action drag and flags drag
+    }
+
+    // called when user drag
+    @Override
+    public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+        callbackItemTouch.itemTouchOnMove(viewHolder.getAdapterPosition(),target.getAdapterPosition()); // information to the interface
+        return true;
+    }
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+        // swiped disabled
+    }
+}
